807032466ef9b4f16dea61fa7be5772161e944dc
Mirna Haddad <mirna.haddad@pavocom.com>
2025-10-10 20:18:55 +0300

    Implemented the design for the signup, added a variables css file to include the colors for our website , and did some adjustments in the login desgin

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,16 +1,29 @@
-from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
-    age = forms.IntegerField(required=True,label='Age')
+    age = forms.IntegerField(
+        required=True,
+        label='Age',
+        min_value=0,
+        help_text="You must be at least 18 years old to sign up.",
+        widget=forms.NumberInput(attrs={
+            'inputmode': 'numeric',
+            'autocomplete': 'off',
+            'min':0
+        })
+    )
+
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2', 'age']
+
     def clean_age(self):
-        age=self.cleaned_data.get('age')
-        if age is not None and age < 18:
-            raise ValidationError('You must be at least 18 years old to sign up.')
-        else:
-            return age
+        cleaned_data = super().clean()
+        age = cleaned_data.get('age')
+
+        if age is None or age < 18:
+            # Add a non-field error instead of a field error
+            self.add_error(None, ValidationError("Form is invalid. Please read the rule carefully."))
